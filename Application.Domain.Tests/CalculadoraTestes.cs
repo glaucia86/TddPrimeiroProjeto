5de35dbcd8f3b8b3766943ff341210065b11b05b
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-03-15 02:16:36 -0300

    First commit

---
+++ Application.Domain.Tests/CalculadoraTestes.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace Application.Domain.Tests
+{
+    [TestFixture]
+    public class CalculadoraTestes
+    {
+        [Test]
+        public void DevoSomarDoisNumeros()
+        {
+            var sut = new Calculadora();
+
+            var result = sut.Somar(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void DevoMultiplicarDoisNumeros()
+        {
+            var sut = new Calculadora();
+
+            var result = sut.Multiplicar(2,10);
+
+            Assert.That(result, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void DevoDiminuirDoisNumeros()
+        {
+            var sut = new Calculadora();
+
+            var result = sut.Diminuir(10, 6);
+
+            Assert.That(result, Is.EqualTo(4));
+        }
+    }
+}
---
+++ Application.Domain/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Domain
+{
+    public class Calculadora
+    {
+        public int Somar(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+
+        public int Multiplicar(int numero1, int numero2)
+        {
+            return numero1 * numero2;
+        }
+
+        public int Diminuir(int numero1, int numero2)
+        {
+            return numero1 - numero2;
+        }
+    }
+}
